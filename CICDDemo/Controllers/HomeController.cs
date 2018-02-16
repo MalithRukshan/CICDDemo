102d2d21f4c3c6e386e3ad645956a3c118c07a9c
SL\mrp <malitharp90@gmail.com>
2018-02-16 17:59:08 +0530

    add hhh

--- CICDDemo/Controllers/HomeController.cs
+++ CICDDemo/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace CICDDemo.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page hhhhhhhhh.";
 
             return View();
         }
